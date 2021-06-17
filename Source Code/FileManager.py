5a70bf8798e9e0bbae61c0598e84da0633866667
michelle zhang <83652383+michellezhang5868@users.noreply.github.com>
2021-06-17 18:22:02 -0400

    Update FileManager.py

--- Source Code/FileManager.py
+++ Source Code/FileManager.py
@@ -3,18 +3,18 @@ from tkinter import filedialog, END, INSERT;
 class FileManager:
     # Open the existing text file
     def open_file(self, text):
-        # Open the dialog and ask the user to select a file
+        # Open  dialog and ask the user to select a file
         existing_file = filedialog.askopenfile(filetypes=[('Text File', '.txt'), ('Python File', '.py')])
-        # Need clear the existing text in the window first
+        # Clear the existing text in the window first
         text.delete(1.0, END)
-        # Then we can insert the new text
+        # Insert the new text
         text.insert(INSERT, existing_file.read())
 
     # Save the text to a file for future usage
     def save_file(self, text):
-        # Open the dialog and ask the user to enter the file name
+        # Open dialog and ask the user to enter the file name
         new_file_path = filedialog.asksaveasfilename(filetypes=[('Text File', '.txt'), ('Python File', '.py')], defaultextension=".txt")
         new_file_name = new_file_path
-        # Create the file with the text
+        # Create the file with the text the user configured
         write = open(new_file_name, mode='w')
         write.write(text.get(1.0, END))
