7f64c76aed8ecbe8fcc6741366c54a12f97ccbfc
Ub1k1 <63819671+Ub1k1@users.noreply.github.com>
2021-06-17 22:50:50 -0400

    Update UI.py

--- Source Code/UI.py
+++ Source Code/UI.py
@@ -119,7 +119,12 @@ class UI:
         voice_speed.pack(in_=toolbar_three, side=LEFT, padx=2, pady=2)
         # Read Out
         play_image = PhotoImage(file="icons/Sound.png")
-        play_button = Button(text="Play", image=play_image, compound=LEFT, width=60, height=24, command=self.buttonFunctions.play_text(text=text, voice_speed=voice_speed, voice_type_combo=voice_type_combo))
+        play_button = Button(text="Play", image=play_image, compound=LEFT, width=60, height=24, command=play_text)
         play_button.pack(in_=toolbar_three, side=LEFT, padx=2, pady=2)
+        pause_image = PhotoImage(file="icons/Pause.png")
+        # Stop Reading
+        stop_image = PhotoImage(file="icons/Stop/png")
+        stop_button = Button(text="Stop", image=stop_image, compound=LEFT, width=60, height=24, command=stop_text)
+        stop_button.pack(in_=toolbar_three, side=LEFT, padx=2, pady=2)
 
         root.mainloop()
