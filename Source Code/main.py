e110e9ae7acdd3a9ba4e03f3485c61a43fcf1dc7
michelle zhang <83652383+michellezhang5868@users.noreply.github.com>
2021-06-18 09:02:05 -0400

    Update main.py

--- Source Code/main.py
+++ Source Code/main.py
@@ -1,9 +1,3 @@
-# tkinter is the standard Python interface to the Tk GUI toolkit.
-# https://docs.python.org/3/library/tkinter.html
-
-# pyttsx3 is a text-to-speech conversion library in Python.
-# https://pypi.org/project/pyttsx3/
-
 from UI import *
 
 ui = UI
