0de19773de60eb555ef80fb14d83dea9501a289b
Ub1k1 <63819671+Ub1k1@users.noreply.github.com>
2021-06-18 20:26:37 -0400

    added docstrings in sphinx format

--- Source Code/Unseparated/Zesty_VersionFive.py
+++ Source Code/Unseparated/Zesty_VersionFive.py
@@ -32,115 +32,115 @@ root.minsize(800, 700)
 tag_sequence = 0
 paused = False
 
-# Open the existing text file
 def open_file():
-    # Open the dialog and ask the user to select a file
+    """Opens a file explorer window that asks the user to select an existing text file,
+    and assigns it to the string variable text.
+    """
     existing_file = filedialog.askopenfile(filetypes=[('Text File', '.txt'), ('Python File', '.py')])
-    # Clear the existing text in the window first
     text.delete(1.0, END)
-    # Insert the new text
     text.insert(INSERT, existing_file.read())
 
-# Save the text to a file for future use
+
 def save_file():
-    # Open the dialog and ask the user to enter the file name
+    """Opens a file explorer window that allows the user to save the string variable text as a new txt
+    file.
+    """
     new_file_path = filedialog.asksaveasfilename(filetypes=[('Text File', '.txt'), ('Python File', '.py')], defaultextension=".txt")
     new_file_name = new_file_path
-    # Create the file with the text
     write = open(new_file_name, mode='w')
     write.write(text.get(1.0, END))
 
 
-# Mark the selected text for later
 def copy_text():
-    # Clear the clipboard first
+    """Copies the selected text to the clipboard.
+    """
     root.clipboard_clear()
-    # Append the selected text to the clipboard
     text.clipboard_append(text.selection_get())
 
 
-# Cut the selected text
 def cut_text():
-    # Clear the clipboard first
+    """Copies the selected text to the clipboard, then deletes the selected text from the string variable
+    text.
+    """
     root.clipboard_clear()
-    # Append the selected text to the clipboard
     text.clipboard_append(text.selection_get())
-    # Delete the selected text
     text.delete(SEL_FIRST, SEL_LAST)
 
 
-# Paste the selected text
 def paste_text():
-    # Paste the selected text at the cursor
+    """Pastes the current clipboard text to the string variable text at the current cursor location.
+    Then clears the clipboard.
+    """
     text.insert(INSERT, root.clipboard_get())
     root.clipboard_clear()
 
 
-# To undo the change
 def undo_text():
+    """Undoes the last change to the text that wasn't an undo.
+    """
     text.edit_undo()
 
 
-# To redo the change
 def redo_text():
+    """If the last change to the text was an undo, reverses the effect of that undo.
+    """
     text.edit_redo()
 
 
-# Search the text and highlight words 
 def find_text():
-    # Create a popup window for search function
+    """Creates a popup window that allows the user to type in the passage they want to search for.
+    """
     search_popup = Toplevel(root)
     search_popup.title('Word Search')
     search_popup.transient(root)
     search_popup.resizable(False, False)
-    # Initial position from top left corner is 800 pixels right and 75 pixels down
     search_popup.geometry("+800+75")
-    # The popup window contains a Label and Entry to allow users to enter the word
     Label(search_popup, text="Enter a word: ").grid(row=0, column=0)
     search_input = Entry(search_popup, width=20)
     search_input.grid(row=0, column=1, padx=2)
     search_input.focus_set()
-    # Action button to search or cancel
-    # If you need to pass a parameter to the function, must use 'lambda:' keyword
     Button(search_popup, text="Search", command=lambda: find_text_search(search_input.get())).grid(row=1, column=0, padx=2, pady=2, sticky="E")
     Button(search_popup, text="Close", command=lambda: find_text_close(search_popup)).grid(row=1, column=1, padx=2, pady=2, sticky="W")
 
 
-# Search the text from the beginning to the end. If the word found, highlight it in yellow
 def find_text_search(word):
+    """Searches the entire text for a passage, and turns the text into purple for all occurrences of the
+    passage. Not case sensitive.
+
+    :param word: the passage to be searched for in the text, defaults to None
+    :type word: string, optional
+    """
     if len(word) == 0:
         return
 
     text.tag_remove("found_text", 1.0, END)
     text.tag_config("found_text", foreground="purple")
-    # Search the text with nocase=1 (ignore case, case insensitive)
     index = 1.0
     while index:
         index = text.search(word, nocase=1, index=index, stopindex=END)
         if index:
-            # Text index is following the row.column format. Means if the starting point is 1.277 (row 1, column 277),
-            # and the search keyword is 3 characters long, then the new index is 1.277+3c (means row 1, column 277 plus 3 characters)
             new_index = str(index) + "+" + str(len(word)) + "c"
             text.tag_add('found_text', index, new_index)
             index = new_index
 
 
-# Remove the highlight and close the search popup window
 def find_text_close(search_popup):
+    """Removes the highlight and closes the search popup window.
+    :param search_popup: the search popup window to be closed
+    :type search_popup: Toplevel
+    """
     text.tag_remove("found_text", 1.0, END)
     search_popup.destroy()
 
 
-# Popup window for how to use the application
 def help_popup():
-    # Create a popup window for the help function
+    """Creates a popup window that explains how to use the application.
+    """
     help_panel = Toplevel(root)
     help_panel.title('Hep')
     help_panel.transient(root)
     help_panel.resizable(False, False)
-    # Initial position from top left corner is 250 pixels right and 200 pixels down
     help_panel.geometry("550x450+250+200")
-    # Help items for the first toolbar
     create_help_item(help_panel, 0, "Open:", "Open an existing text file and configure it to your liking!")
     create_help_item(help_panel, 1, "Save:", "Save the text to a new file")
     create_help_item(help_panel, 2, "Copy:", "Save the highlighted text to the clipboard")
@@ -149,42 +149,47 @@ def help_popup():
     create_help_item(help_panel, 5, "Undo:", "Undo a text change")
     create_help_item(help_panel, 6, "Redo:", "Redo a text change")
     create_help_item(help_panel, 7, "Find:", "Find the word you entered within the text")
-    # Help items for the second toolbar
     create_help_item(help_panel, 8, "------", "")
     create_help_item(help_panel, 9, "Font:", "Change the font type and size for the highlighted text")
     create_help_item(help_panel, 10, "Bold:", "Make the highlighted text bold")
     create_help_item(help_panel, 11, "Italic:", "Italicize the highlighted text")
     create_help_item(help_panel, 12, "Underline:", "Underline the highlighted text")
     create_help_item(help_panel, 13, "Color:", "Change the color of the highlighted text")
-    # Help items for the third toolbar
     create_help_item(help_panel, 14, "------", "")
     create_help_item(help_panel, 15, "Select your preferred voice type, change the playback speed, and play it outloud using the play button")
 
 
 # Create each help item by passing in the parent widget and row number, with the function name and description of the function
 def create_help_item(parent_widget, row_number, function_name, function_description):
+    """Create each help item by passing in the parent widget and row number,
+    with the function name and description of the function.
+
+    :param parent_widget: The popup window that the help item is to be created in
+    :type parent_widget: Toplevel
+    :param row_number: The row order of appearance of the help item in the help window
+    :type row_number: int
+    :param function_name: The name of the button whose function is being explained
+    :type function_name: string
+    :param function_description: The description of the button's function
+    :type function_description: string
+    """
     Label(parent_widget, text=function_name, font="Arial 12 bold").grid(row=row_number, column=0, padx=2, pady=2, sticky="W")
     Label(parent_widget, text=function_description, font="Arial 12").grid(row=row_number, column=1, padx=20, pady=2, sticky="W")
 
 
-# Make the highlighted text bold
 def bold_text():
-    # Global tag_sequence, so any change to tag_sequence will be visible from other methods
+    """If the selected text is not bold, then makes it bold. Otherwise, makes it unbold.
+    """
     global tag_sequence
-    # Check if there's any selected text. If no text is selected, no need for further action
     if text.tag_ranges("sel"):
-        # Get a list of existing tag for the selected text
         tags = text.tag_names(SEL_FIRST)
         tag_found = 0
-        # Check if the selected text has a bold tag
         for tag_name in tags:
             if tag_name.startswith("bold_text_"):
                 tag_found = 1
         if tag_found == 1:
-            # If the selected text already has a bold tag, then make the text un-bolded
             text.tag_remove(tag_name, SEL_FIRST, SEL_LAST)
         else:
-            # If the selected text does not have a bold tag, then make the text bolded
             font_style = font.Font(text, text.cget("font"))
             font_style.configure(weight="bold")
             text.tag_add("bold_text_" + str(tag_sequence), SEL_FIRST, SEL_LAST)
@@ -192,24 +197,20 @@ def bold_text():
             tag_sequence += 1
 
 
-# Make the highlighted text italized
 def italic_text():
+    """If the selected text is not italicized, then makes it italicized. Otherwise, makes it unitalicized.
+    """
     global tag_sequence
-    # Check if there's any selected text. If no text is selected, no need for further action
     if text.tag_ranges("sel"):
-        # Get a list of existing tag for the selected text
         tags = text.tag_names(SEL_FIRST)
         tag_found = 0
-        # Check if the selected text has a italic tag
         for tag_name in tags:
             if tag_name.startswith("italic_text_"):
                 tag_found = 1
                 current_tag_name = tag_name
         if tag_found == 1:
-            # If the selected text already has a italic tag, then make the text un-italized
             text.tag_remove(tag_name, SEL_FIRST, SEL_LAST)
         else:
-            # If the selected text does not have a italic tag, then make the text italized
             font_style = font.Font(text, text.cget("font"))
             font_style.configure(slant="italic")
             text.tag_add("italic_text_" + str(tag_sequence), SEL_FIRST, SEL_LAST)
@@ -217,23 +218,19 @@ def italic_text():
             tag_sequence += 1
 
 
-# Add underline to the highlighted text
 def underline_text():
+    """If the selected text is not underlined, then makes it underlined. Otherwise, makes it not underlined.
+    """
     global tag_sequence
-    # Check if there's any selected text. If no text is selected, no need for further action
     if text.tag_ranges("sel"):
-        # Get a list of existing tag for the selected text
         tags = text.tag_names(SEL_FIRST)
         tag_found = 0
-        # Check if the selected text has a underline tag
         for tag_name in tags:
             if tag_name.startswith("underline_text_"):
                 tag_found = 1
         if tag_found == 1:
-            # If the selected text already has a underline tag, then remove the underline
             text.tag_remove(tag_name, SEL_FIRST, SEL_LAST)
         else:
-            # If the selected text does not have a underline tag, then add the underline
             font_style = font.Font(text, text.cget("font"))
             font_style.configure(underline=1)
             text.tag_add("underline_text_" + str(tag_sequence), SEL_FIRST, SEL_LAST)
@@ -241,23 +238,19 @@ def underline_text():
             tag_sequence += 1
 
 
-# Change the font type of the selected text
 def change_text_type(event):
+    """Changes the font type of the selected text to a user-defined font face.
+    """
     global tag_sequence
-    # Check if there's any selected text. If no text is selected, no need for further action
     if text.tag_ranges("sel"):
-        # Get a list of existing tag for the selected text
         tags = text.tag_names(SEL_FIRST)
         tag_found = 0
-        # Check if the selected text has a font type tag
         for tag_name in tags:
             if tag_name.startswith("font_type_text_"):
                 tag_found = 1
         if tag_found == 1:
-            # If the selected text already has a font type tag, then remove the font type
             text.tag_remove(tag_name, SEL_FIRST, SEL_LAST)
         else:
-            # If the selected text does not have a font type tag, then add the font type
             font_style = font.Font(text, text.cget("font"))
             font_style.configure(family=font_type.get())
             text.tag_add("font_type_text_" + str(tag_sequence), SEL_FIRST, SEL_LAST)
@@ -265,24 +258,19 @@ def change_text_type(event):
             tag_sequence += 1
 
 
-# Change the font size of the selected text
 def change_text_size(event):
-    # We need make the tag_sequence as global, so any change to tag_sequence will be visible from other methods
+    """Changes the font size of the selected text to a user-defined font size.
+    """
     global tag_sequence
-    # Check if there's any selected text. If no text is selected, no need for further action
     if text.tag_ranges("sel"):
-        # Get a list of existing tag for the selected text
         tags = text.tag_names(SEL_FIRST)
         tag_found = 0
-        # Check if the selected text has a font size tag
         for tag_name in tags:
             if tag_name.startswith("font_size_text_"):
                 tag_found = 1
         if tag_found == 1:
-            # If the selected text already has a font size tag, then remove the font size
             text.tag_remove(tag_name, SEL_FIRST, SEL_LAST)
         else:
-            # If the selected text does not have a font size tag, then add the font size
             font_style = font.Font(text, text.cget("font"))
             font_style.configure(size=font_size.get())
             text.tag_add("font_size_text_" + str(tag_sequence), SEL_FIRST, SEL_LAST)
@@ -290,34 +278,33 @@ def change_text_size(event):
             tag_sequence += 1
 
 
-# Change the colour of the selected text
 def change_text_color():
-    # We need make the tag_sequence as global, so any change to tag_sequence will be visible from other methods
+    """Changes the font colour of the selected text to a user-defined font colour.
+    """
     global tag_sequence
-    # Open the color chooser dialog
     color = colorchooser.askcolor(initialcolor="red")
     color_name = color[1]
-    # Check if there's any selected text. If no text is selected, no need for further action
     if text.tag_ranges("sel"):
-        # Get a list of existing tag for the selected text
         tags = text.tag_names(SEL_FIRST)
         tag_found = 0
-        # Check ig the selected text has a font size tag
         for tag_name in tags:
             if tag_name.startswith("font_color_text_"):
                 tag_found = 1
         if tag_found == 1:
-            # If the selected text already has a font color tag, then remove the font colour
             text.tag_remove(tag_name, SEL_FIRST, SEL_LAST)
         else:
-            # If the selected text does not have a font color tag, then add the font colour
             text.tag_add("font_color_text_" + str(tag_sequence), SEL_FIRST, SEL_LAST)
             text.tag_configure("font_color_text_" + str(tag_sequence), foreground=color_name)
             tag_sequence += 1
 
 
-# Play the text user highlighted, otherwise, play all the text
 def play_text(play_button):
+    """If no audio is in queue to be played, then starts playing the selected text. If no text is selected, plays the entire text. If audio is being played, pauses the audio
+    and updates button image and text to match "Pause". If the audio is currently paused, resumes the audio and updates button image and text to match "Play".
+    
+    :param play_button: The play/pause button
+    :type play_button: Button
+    """
     global paused
 
     print('paused =', paused, ', button text =', play_button['text'], ', busy =', pygame.mixer.music.get_busy())
@@ -347,6 +334,11 @@ def play_text(play_button):
 
 
 def stop_text(play_button):
+    """Stops the current audio and clears the audio queue.
+    
+    :param play_button: The play/pause button
+    :type play_button: Button
+    """
     pygame.mixer.music.stop()
     global paused
     paused = False
